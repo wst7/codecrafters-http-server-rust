e609c8a5ebc6c8635b561bcb9f7b049d1b9860d2
wstreet7 <wstreet7@outlook.com>
2024-11-18 16:53:32 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io::Write;
-#[allow(unused_imports)]
+use std::io::{BufRead, BufReader, Write};
 use std::net::TcpListener;
+
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,13 +12,51 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                println!("accepted new connection");
-                let _ = _stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
+            Ok(mut stream) => {
+                handle_connection(&mut stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+// GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n
+
+// Request line
+// GET                          // HTTP method
+// /index.html                  // Request target
+// HTTP/1.1                     // HTTP version
+// \r\n                         // CRLF that marks the end of the request line
+
+// // Headers
+// Host: localhost:4221\r\n     // Header that specifies the server's host and port
+// User-Agent: curl/7.64.1\r\n  // Header that describes the client's user agent
+// Accept: */*\r\n              // Header that specifies which media types the client can accept
+// \r\n                         // CRLF that marks the end of the headers
+
+// // Request body (empty)
+
+fn handle_connection(stream: &mut std::net::TcpStream) {
+    let buf_reader = BufReader::new(&mut *stream);
+
+    let http_request: Vec<_> = buf_reader
+    .lines()
+    .map(|line| line.unwrap())
+    .take_while(|x| !x.is_empty())
+    .collect();
+
+    let request_line = http_request[0].split(" ").collect::<Vec<&str>>();
+
+    match request_line[1] {
+        "/" => {
+            let response = "HTTP/1.1 200 OK\r\n\r\nHello, world";
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+        _ => {
+            let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+    }
+
 }
